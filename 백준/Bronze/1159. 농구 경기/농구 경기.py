2b7faadd7549fe26549f2aca6bb71a7d434c0321
ssj7509 <87068596+ssj7509@users.noreply.github.com>
2023-12-22 18:08:06 +0900

    [Bronze II] Title: 농구 경기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1159. 농구 경기/농구 경기.py
@@ -0,0 +1,7 @@
+import sys
+inp=sys.stdin.readline
+L=[0]*26
+for _ in range(int(inp())):
+    L[ord(inp().strip()[0])-97]+=1
+r=''.join([chr(i+97)for i,n in enumerate(L) if n>=5])
+print((r,"PREDAJA")[len(r)==0])
