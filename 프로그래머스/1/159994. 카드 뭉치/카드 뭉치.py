ad5cbe694124843424c663d3625cea4069505418
ssj7509 <87068596+ssj7509@users.noreply.github.com>
2023-12-27 21:57:33 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,13 @@
+def solution(cards1, cards2, goal):
+    i1,i2=0,0
+    t=0
+    for g in goal:
+        if i1<len(cards1) and cards1[i1]==g:
+            i1+=1
+        elif i2<len(cards2) and cards2[i2]==g:
+            i2+=1
+        else:
+            t=1
+            break
+    answer = ("Yes","No")[t]
+    return answer
