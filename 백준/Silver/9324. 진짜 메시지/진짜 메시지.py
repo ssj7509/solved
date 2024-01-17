1261ffda4b1a1deeef85baa623551eb8088ee6b6
ssj7509 <87068596+ssj7509@users.noreply.github.com>
2024-01-17 22:37:41 +0900

    [Silver V] Title: 진짜 메시지, Time: 508 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9324. 진짜 메시지/진짜 메시지.py
+++ 백준/Silver/9324. 진짜 메시지/진짜 메시지.py
@@ -1,20 +1,12 @@
 import sys
 f=sys.stdin.readline
-
 for _ in range(int(f())):
     s=f().strip()
     L=[0]*26
     t=1
-    
     for i in range(len(s)):
         tn=ord(s[i])-65
         L[tn]+=1
-        '''
-        if L[tn]==3 and (i==len(s)-1 or s[i]!=s[i+1]):
-            print("FAKE")
-            t=0
-            break
-        '''
         if L[tn]==3:
             if (i==len(s)-1 or s[i]!=s[i+1]):
                 print("FAKE")
@@ -22,6 +14,5 @@ for _ in range(int(f())):
                 break
             else:
                 L[tn]=-1
-
     if t:        
         print("OK")
