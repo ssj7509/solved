975284dce2360ae9357be3dffaaac53f8ba6c6d1
ssj7509 <87068596+ssj7509@users.noreply.github.com>
2024-01-24 15:38:08 +0900

    [Silver V] Title: 세 부분, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2993. 세 부분/세 부분.py
+++ 백준/Silver/2993. 세 부분/세 부분.py
@@ -1,13 +1,7 @@
-def f(s):
-    l=len(s)
-    L=list(s)
-    for i in range(l//2):
-        L[i],L[l-i-1]=L[l-i-1],L[i]
-    return ''.join(L)
 s=input()
 r=[]
 for i in range(1,len(s)-1):
     for j in range(i+1,len(s)):
-        t1,t2,t3=map(f,(s[:i],s[i:j],s[j:]))
+        t1,t2,t3=map(lambda t:t[::-1],(s[:i],s[i:j],s[j:]))
         r.append(t1+t2+t3)
 print(sorted(r)[0])
