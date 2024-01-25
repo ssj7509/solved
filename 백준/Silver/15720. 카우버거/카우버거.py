ed1ace0d6618a935516cbc59277697dead8c2738
ssj7509 <87068596+ssj7509@users.noreply.github.com>
2024-01-25 21:03:58 +0900

    [Silver V] Title: 카우버거, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15720. 카우버거/카우버거.py
+++ 백준/Silver/15720. 카우버거/카우버거.py
@@ -1,7 +1,7 @@
 import sys
-f=lambda:map(int,sys.stdin.readline().split())
+f=lambda:sorted(map(int,sys.stdin.readline().split()))
 B,C,D=f()
-BL,CL,DL=[*f()],[*f()],[*f()]
+BL,CL,DL=f(),f(),f()
 p=min(B,C,D)
 s=sum(BL+CL+DL)
-print(s,s-sum(map(lambda L:sum(sorted(L)[-p:])//10,(BL,CL,DL))),sep='\n')
+print(s,s-sum(map(lambda L:sum(L[-p:])//10,(BL,CL,DL))),sep='\n')
