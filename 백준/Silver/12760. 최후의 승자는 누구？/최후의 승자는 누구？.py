ebca9b33d91a74b664945e005e2e22223bf92092
ssj7509 <87068596+ssj7509@users.noreply.github.com>
2024-01-31 00:47:00 +0900

    [Silver V] Title: 최후의 승자는 누구?, Time: 124 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/12760. 최후의 승자는 누구？/최후의 승자는 누구？.py
+++ 백준/Silver/12760. 최후의 승자는 누구？/최후의 승자는 누구？.py
@@ -3,5 +3,6 @@ f=lambda:map(int,sys.stdin.readline().split())
 n,m=f()
 nL=[[*sorted(f())]for _ in range(n)]
 mxL=[max(L[i] for L in nL)for i in range(m)]
-cL=[len([i for i in range(m)if L[i]==mxL[i]])for L in nL];mx=max(cL)
+cL=[len([i for i in range(m)if L[i]==mxL[i]])for L in nL]
+mx=max(cL)
 print(*[i+1 for i,n in enumerate(cL)if n==mx])
