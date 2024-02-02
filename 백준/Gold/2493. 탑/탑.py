9e986d877b5842d86589dc0b4e8b67642edbd1fe
ssj7509 <87068596+ssj7509@users.noreply.github.com>
2024-02-03 02:18:03 +0900

    [Gold V] Title: 탑, Time: 640 ms, Memory: 114664 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,14 +1,16 @@
-from bisect import bisect_right as b
 N=int(input())
 L=[*map(int,input().split())]
 r=[0]*N
 st=[]
 for i,n in enumerate(L):
-    p=b(st,(-n,i+1))-1
-    tn,ti=st[p]if p<len(st)and p>=0 else (0,0)
-    if p<len(st):
-        r[i]=ti
-    for j in range(len(st)-p-1):
-        st.pop()
-    st.append((-n,i+1))
+    while True:
+        if not st:
+            break
+        if st[-1][0]<n:
+            st.pop()
+        else:
+            break
+    if st:
+        r[i]=st[-1][1]
+    st.append((n,i+1))
 print(*r)
