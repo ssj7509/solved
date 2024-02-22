897f3b89b5abf9df0c6c073f1b20fbb71fb9dfee
ssj7509 <87068596+ssj7509@users.noreply.github.com>
2024-02-22 21:35:39 +0900

    [Silver IV] Title: 디지털 친구, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1985. 디지털 친구/디지털 친구.py
+++ 백준/Silver/1985. 디지털 친구/디지털 친구.py
@@ -1,10 +1,11 @@
-def f(s,t1,t2,t3,d):
+def f(s,t1,t2,d):
+    q,p=t2-t1,t1-t2
     for i in range(len(s)-1):
         n1,n2=s[i],s[i+1]
         v=(n1==n2)+1
-        t4,t5={n1-1,n2+1},{n1+1,n2-1}
-        v2=not(t2-{n for n in (n1,n2)if n in t2 and d[n]==v}) and (n1 in t2)^(d[n1]>v) and (n2 in t2)^(d[n2]>v)
-        if v2 and ((n1>1-(i>0) and n2<9 and t4>=t1 and not t4-t3) or (n1<9 and n2>0 and t5>=t1 and not t5-t3)):
+        t3,t4={n1-1,n2+1},{n1+1,n2-1}
+        v2=not(p-{n for n in (n1,n2)if n in p and d[n]==v}) and (n1 in p)^(d[n1]>v) and (n2 in p)^(d[n2]>v)
+        if v2 and ((n1>1-(i>0) and n2<9 and t3>=q and not t3-t2) or (n1<9 and n2>0 and t4>=q and not t4-t2)):
             return 1
     return 0
 def g(s):
@@ -18,4 +19,4 @@ for _ in range(3):
     a,b=map(lambda s:[*map(int,s)],input().split())
     d1,d2=map(g,(a,b))
     t1,t2=map(lambda d:{*d.keys()},(d1,d2))
-    print(('friends',('nothing','almost friends')[f(a,t2-t1,t1-t2,t2,d1) or f(b,t1-t2,t2-t1,t1,d2)])[t1!=t2])
+    print(('friends',('nothing','almost friends')[f(a,t1,t2,d1) or f(b,t2,t1,d2)])[t1!=t2])
