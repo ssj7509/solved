ecb6003d362bbea253a47e0a84e35d13adb92508
ssj7509 <87068596+ssj7509@users.noreply.github.com>
2024-02-25 03:47:01 +0900

    [Silver IV] Title: 김밥 21개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/3254. 김밥 21개/김밥 21개.py
@@ -0,0 +1,24 @@
+V,L,t,r=((1,0),(0,1),(1,1),(1,-1)),[[0]*7 for _ in range(6)],[5]*7,'ss'
+for i in range(21):
+    X=[*map(int,input().split())]
+    s=0
+    for k in (0,1):
+        x,y=X[k]-1,t[X[k]-1]
+        t[X[k]-1]-=1
+        L[y][x]=k+1
+        for xv,yv in V:
+            sx,sy,s2=x-6*xv,y-6*yv,0
+            for j in range(13):
+                tx,ty=sx+j*xv,sy+j*yv
+                s2=0<=tx<7 and 0<=ty<6 and L[ty][tx]==k+1 and s2+1
+                if s2>=4:
+                    print(('sk','ji')[k],i+1)
+                    quit()
+            if s2>=4:
+                s=k+1
+                break
+        if s:
+            break
+    if s:
+        break
+print('ss')
