d25106bc240d41a220a78b2b737296f5409c7273
ssj7509 <87068596+ssj7509@users.noreply.github.com>
2024-02-26 22:03:51 +0900

    [Silver IV] Title: 볼질, Time: 68 ms, Memory: 31988 KB -BaekjoonHub

--- 백준/Silver/11916. 볼질/볼질.py
+++ 백준/Silver/11916. 볼질/볼질.py
@@ -4,7 +4,7 @@ for n in L:
     b+=n!=2
     if b==4 or n in (2,3):
         v=n==3 and b<4
-        t=((t|(t+1))>>1<<1,t<<1)[n==3]+1-v
+        t=(((t|(t+1))>>1,t)[n==3]<<1)+1-v
         b=v and b
     if t>7:
         r+=1
