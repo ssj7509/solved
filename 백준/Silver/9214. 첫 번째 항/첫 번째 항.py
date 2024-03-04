d11adff8efff4a345a3456d11b13b63cfb06bb15
ssj7509 <87068596+ssj7509@users.noreply.github.com>
2024-03-05 00:15:42 +0900

    [Silver V] Title: 첫 번째 항, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9214. 첫 번째 항/첫 번째 항.py
+++ 백준/Silver/9214. 첫 번째 항/첫 번째 항.py
@@ -6,7 +6,7 @@ while True:
         break
     while 1-len(s)%2:
         t=''.join(s[j+1]*int(s[j])for j in range(0,len(s),2))
-        if s==t or sum(s[j]==s[j+2]for j in range(1,len(s)-2,2)):
+        if s==t:
             break
         s=t
     print(f"Test {i}: {s}")
