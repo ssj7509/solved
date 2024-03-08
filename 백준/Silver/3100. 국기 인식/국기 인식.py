5a72329e2e5902d368b8a16b9097aeeae6b29dba
ssj7509 <87068596+ssj7509@users.noreply.github.com>
2024-03-09 02:08:25 +0900

    [Silver IV] Title: 국기 인식, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/3100. 국기 인식/국기 인식.py
+++ 백준/Silver/3100. 국기 인식/국기 인식.py
@@ -5,4 +5,4 @@ for i in range(6):
         R[i//2][n]-=1
         R[j//3+3][n]-=1
 R,T=[*map(lambda x:sorted(enumerate(x),key=lambda x:x[1]),R)],(0,1,2)
-print(min(54,*[sum(r[t][1]for r,t in zip(tr,(i,j,k)))for tr,l in zip((R[:3],R[3:]),(0,3)) for k in T for j in T for i in T if R[0+l][i][0]!=R[1+l][j][0] and R[1+l][j][0]!=R[2+l][k][0]]))
+print(min(54,*[sum(r[t][1]for r,t in zip(tr,(i,j,k)))for tr,l in zip((R[:3],R[3:]),(0,3))for k in T for j in T for i in T if R[0+l][i][0]!=R[1+l][j][0] and R[1+l][j][0]!=R[2+l][k][0]]))
