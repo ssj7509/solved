389d3d008f302a60262c76abac0ffbc18a9e80ba
ssj7509 <87068596+ssj7509@users.noreply.github.com>
2024-03-12 00:13:14 +0900

    [Silver V] Title: 배열 합치기, Time: 1516 ms, Memory: 196224 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.py
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -1,6 +1,3 @@
-f=lambda:map(int,input().split())
-N,M,A,B,iL=*f(),[*f()],[*f()],[0,0]
-for i in range(N+M):
-    t=iL[1]<M and (iL[0]==N or A[iL[0]]>B[iL[1]])
-    print((A,B)[t][iL[t]],end=' ')
-    iL[t]+=1
+f=lambda:[*map(int,input().split())]
+f()
+print(*sorted(f()+f()))
