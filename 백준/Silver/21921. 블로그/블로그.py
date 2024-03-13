384307921893dfa66f0bcfe6c2f7c56aabf6ca03
ssj7509 <87068596+ssj7509@users.noreply.github.com>
2024-03-14 02:14:23 +0900

    [Silver III] Title: 블로그, Time: 268 ms, Memory: 58732 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.py
+++ 백준/Silver/21921. 블로그/블로그.py
@@ -1,7 +1,6 @@
-N,M,L,s,r,c=*map(int,input().split()),[*map(int,input().split())],0,0,0
+s,r,c,N,M,*L=0,0,0,*map(int,input().split()+input().split())
 for i,n in enumerate(L):
-    s+=n
-    s-=i-M>=0 and L[i-M]
+    s+=n-(i-M>=0 and L[i-M])
     t=i>=M-1 and s>=r
     r,c=max(r,t and s),(s==r and c)+t or c
-print(*(('SAD',),(r,c))[r>0],sep='\n')
+print(('SAD',f'{r}\n{c}')[r>0])
