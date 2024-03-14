ccad82a1d9b71c475d84a929369fe39aea67d4c5
ssj7509 <87068596+ssj7509@users.noreply.github.com>
2024-03-15 04:40:53 +0900

    [Gold III] Title: 현욱은 괄호왕이야!!, Time: 108 ms, Memory: 32704 KB -BaekjoonHub

--- 백준/Gold/15926. 현욱은 괄호왕이야！！/현욱은 괄호왕이야！！.py
+++ 백준/Gold/15926. 현욱은 괄호왕이야！！/현욱은 괄호왕이야！！.py
@@ -2,11 +2,11 @@ N,st,m=int(input()),[0],0
 for c in input():
     if len(st)==1 and c==')':
         st=[0]
+        continue
+    if c=='(':
+        st.append(0)
     else:
-        if c=='(':
-            st.append(0)
-        else:
-            t=st.pop()
-            st[-1]+=2+t
-            m=max(m,st[-1])
+        t=st.pop()
+        st[-1]+=2+t
+        m=max(m,st[-1])
 print(m)
