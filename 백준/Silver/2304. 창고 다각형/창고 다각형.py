fa4d6edd95dcef1cc7bab881e22d195dba98c7ef
ssj7509 <87068596+ssj7509@users.noreply.github.com>
2024-03-25 23:44:35 +0900

    [Silver II] Title: 창고 다각형, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -1,15 +1,14 @@
 import sys
 f,st,m,r=sys.stdin.readline,[],0,0
 for x,y in sorted(tuple(map(int,f().split()))for _ in range(int(f()))):
-    d=len(st) and x-st[-1][0]-1
-    r+=y+min(m,y)*d
+    c,d=len(st) and x-st[-1][0],min(m,y)
+    r+=y+d*(c-1)
     if not st or st[-1][1]>y:
-        st.append((x,y,d+1))
+        st.append((x,y,c))
     else:
-        c=d+1
         while st and st[-1][1]<=y:
             w,h,t=st.pop()
-            n,c=min(m,y)-h,c+t
+            n,c=d-h,c+t
             r+=n>0 and n*t
         st.append((x,y,c))
     m=max(m,y)
