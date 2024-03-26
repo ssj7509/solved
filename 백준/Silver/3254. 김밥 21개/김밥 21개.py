baea678a41f65e4b066911a0065d0b1a1dbd0000
ssj7509 <87068596+ssj7509@users.noreply.github.com>
2024-03-26 23:24:32 +0900

    [Silver IV] Title: 김밥 21개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/3254. 김밥 21개/김밥 21개.py
+++ 백준/Silver/3254. 김밥 21개/김밥 21개.py
@@ -1,16 +1,12 @@
-V,L,t=((1,0),(0,1),(1,1),(1,-1)),[[0]*7 for _ in range(6)],[5]*7
+V,L,t=((1,0),(0,1),(1,1),(1,-1)),[[0]*7 for _ in [0]*6],[5]*7
 for i in range(21):
     X=[*map(int,input().split())]
     for k in (0,1):
-        x,y=X[k]-1,t[X[k]-1]
-        t[X[k]-1]-=1
-        L[y][x]=k+1
-        for xv,yv in V:
-            sx,sy=x-6*xv,y-6*yv
+        x,y=X[k]-1,t[X[k]-1];t[X[k]-1]-=1;L[y][x]=k+1
+        for a,b in V:
+            c,d=x-6*a,y-6*b
             for j in range(10):
-                tx,ty=sx+j*xv,sy+j*yv
-                s=0<=tx<7 and 0<=ty<6 and L[ty][tx]==k+1 and s+1
+                e,f=c+j*a,d+j*b;s=0<=e<7 and 0<=f<6 and L[f][e]==k+1 and s+1
                 if s>=4:
-                    print(('sk','ji')[k],i+1)
-                    quit()
+                    print(('sk','ji')[k],i+1);quit()
 print('ss')
