224d9f4f5c7ae6406c04225561a1d7758c4a941f
ssj7509 <87068596+ssj7509@users.noreply.github.com>
2024-04-03 23:08:42 +0900

    [Silver IV] Title: 도미노 무너트리기, Time: 1604 ms, Memory: 191136 KB -BaekjoonHub

--- 백준/Silver/25972. 도미노 무너트리기/도미노 무너트리기.py
+++ 백준/Silver/25972. 도미노 무너트리기/도미노 무너트리기.py
@@ -1,5 +1,5 @@
 import sys
 f,t,r=sys.stdin.readline,0,0
-for a,l in sorted(tuple(map(int,f().split()))for _ in range(int(f()))):
+for a,l in sorted((*map(int,f().split()),)for _ in range(int(f()))):
     r,t=r+(a>t),a+l
 print(r)
