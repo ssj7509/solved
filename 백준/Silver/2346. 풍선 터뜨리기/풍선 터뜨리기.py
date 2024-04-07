a1d4032966fe3f4cd4e01e5be5730d771a2fb2e9
ssj7509 <87068596+ssj7509@users.noreply.github.com>
2024-04-08 01:47:26 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 60 ms, Memory: 34008 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -2,4 +2,4 @@ from collections import deque
 N,d=int(input()),deque(enumerate(map(int,input().split())))
 for _ in range(N):
     n=print(d[0][0]+1,end=' ')or d[0][1]
-    _=d.popleft(),d and[d.append(d.popleft())if n>0 else d.appendleft(d.pop())for _ in range(abs(n)-(n>0))]
+    _=d.popleft(),[d.rotate((n>0)-n)]
