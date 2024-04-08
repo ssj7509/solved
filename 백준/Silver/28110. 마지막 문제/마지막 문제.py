911202727930cf1b5b31aa80349349e59f12c14e
ssj7509 <87068596+ssj7509@users.noreply.github.com>
2024-04-08 23:32:43 +0900

    [Silver IV] Title: 마지막 문제, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/28110. 마지막 문제/마지막 문제.py
+++ 백준/Silver/28110. 마지막 문제/마지막 문제.py
@@ -2,5 +2,5 @@ m,r,N,*L=0,-1,*map(int,[input()]+input().split())
 L.sort()
 for i in range(N-1):
     t=max(m,(L[i+1]-L[i])//2)
-    r,m=((L[i]+L[i+1])//2,t) if m<t else (r,m)
+    r,m=((r,m),((L[i]+L[i+1])//2,t))[m<t]
 print(r)
