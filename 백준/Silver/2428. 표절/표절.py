9a69c10f9d0acb63791f14366bdc5d3e810a2ee6
ssj7509 <87068596+ssj7509@users.noreply.github.com>
2024-04-17 23:09:36 +0900

    [Silver III] Title: 표절, Time: 156 ms, Memory: 44104 KB -BaekjoonHub

--- 백준/Silver/2428. 표절/표절.py
+++ 백준/Silver/2428. 표절/표절.py
@@ -1,4 +1,4 @@
 from bisect import bisect_right as b
-_,*L=map(int,open(0).read().split())
+N,*L=map(int,open(0).read().split())
 L.sort()
-print(sum(b(L,10*n/9,i+1)-i-1 for i,n in enumerate(L)))
+print(sum(b(L,10*L[i]/9,i+1)-i-1 for i in range(N)))
